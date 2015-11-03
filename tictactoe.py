d07c2d75f49fa362c791f4a2b4c47a9a783dc7e2
geoffreyyip <gyip4@binghamton.edu>
2015-11-03 11:43:53 -0500

    Added a program description

--- tictactoe.py
+++ tictactoe.py
@@ -1,1 +1,6 @@
-# Code goes here.
+################################################################################
+# tictactoe.py - the first of many programs
+# uses numpy arrays to represent an internal tictactoe board
+
+# uses four global variables - board_arr, tutorial_arr, user_num, and comp_num
+
